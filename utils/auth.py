92acb4d86f68bd563d28c245751a28499539488d
pixarsart <pixarsart@Python-System>
2025-03-29 06:03:52 +0500

    latest

---
+++ utils/auth.py
@@ -0,0 +1,17 @@
+# from django.core.mail import send_mail
+# from django.conf import settings
+
+# def send_mail_to_user(
+#     recipient_email="test@test.com",
+#     message="If you receive this message contact Developer",
+#     subject="Password Reset"
+# ):
+#     try:
+        
+#         # Send email
+#         result = send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [recipient_email], fail_silently=False)
+
+#         return {"success": "Email sent successfully!"}
+
+#     except Exception as e:
+#         return {"error": str(e)}
